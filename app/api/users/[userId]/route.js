21643518571d1651d4882b1d33f2b3c479eb5106
Krish_Raziel19 <112201013@smail.iitpkd.ac.in>
2024-09-04 11:44:21 +0530

    Chat list display works. Added api

---
+++ app/api/users/[userId]/route.js
@@ -0,0 +1,23 @@
+import User from "@models/User";
+import Chat from "@models/Chat";
+import { connectToDB } from "@mongodb"
+
+export const GET = async (req, { params }) => {
+    try {
+        await connectToDB();
+
+        const { userId } = params;
+        const allChats = await Chat.find({ members: userId })
+        .sort({ lastMessageAt: -1 })
+        .populate({ 
+            path: "members",
+            model: User
+        }).exec();
+
+        return new Response(JSON.stringify(allChats), { status: 200 });
+    } 
+    catch (error) {
+        console.log(error);
+        return new Response("Error: Failed to fetch chats", { status: 500 });
+    }
+}
---
+++ app/api/users/[userId]/searchChat/[query]/route.js
@@ -0,0 +1,20 @@
+import User from "@models/User";
+import Chat from "@models/Chat";
+import { connectToDB } from "@mongodb";
+
+export const GET = async (req, { params }) => {
+    try {
+        await connectToDB();
+        const { userId, query } = params;
+        const searchChats = await Chat.find({
+            members: userId,
+            name: { $regex: query, $options: 'i' }
+        }).populate({ path:'members', model: User}).exec();
+
+        return new Response(JSON.stringify(searchChats), { status: 200 });
+    } 
+    catch (error) {
+        console.log(error);
+        return new Response("Error: Failed to get chat", { status: 500 }); 
+    }
+}
